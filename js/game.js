605239afba1d7077c1d1e1a46cb65ffda46cf71e
OlyKid <anolykid@gmail.com>
2018-06-07 15:28:10 -0700

    js tweaks, css tweaks

--- js/game.js
+++ js/game.js
@@ -43,16 +43,14 @@ function start() {
   $("#username").text(username); // add user name to health bar
   $("#foename").text(foe); // add foe name to health bar
   $("#startbtn").css("display", "none");
-  $("#foe").append("<h5 id='rightlivesheader' class='headers'>Health:</h5><div class='progress' id='lives'><div class='progress-bar' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%' id='rightbar'></div></div><h5 id='rightchargeheader' class='headers'>Charge:</h5><div class='progress' id='charge'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%' id='chargerightbar'></div></div>")
-  $("#user").append("<h5 id='leftlivesheader' class='headers'>Health:</h5><div class='progress' id='lives'><div class='progress-bar' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%' id='leftbar'></div></div><h5 id='rightleftheader' class='headers'>Charge:</h5><div class='progress' id='charge'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%' id='chargeleftbar'></div></div>")
- 
+  $("#foe").append("<h5 id='rightlivesheader' class='headers'>Health:</h5><div class='progress' id='lives'><div class='progress-bar' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%' id='rightbar'></div></div><h5 id='rightchargeheader' class='headers'>Charge:</h5><div class='progress' id='charge'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%' id='chargerightbar'></div></div>");
+  $("#user").append("<h5 id='leftlivesheader' class='headers'>Health:</h5><div class='progress' id='lives'><div class='progress-bar' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%' id='leftbar'></div></div><h5 id='leftchargeheader' class='headers'>Charge:</h5><div class='progress' id='charge'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%' id='chargeleftbar'></div></div>");
 }
 
 function gui(){
   $("#leftplayer").attr("src", "img/stay.PNG").attr("width", staysize);
   $("#rightplayer").attr("src", "img/stay.PNG").attr("width", staysize);
   $(".gui").append("<div id='movepick'><h3>What move are you going to use?<br></h3><button onclick='charge();'><img src='img/electric.png' class='icon'></button><button onclick='attack();'><img src='img/dualsword.png' class='icon'></button><button onclick='block();'><img src='img/shield.png' class='icon'>");
-  $(".bar").css("display", "block");
   $(".headers").css("display", "block");
   
 }
@@ -169,17 +167,17 @@ function resumeGame(){
         $("h4,table,.end").remove();
     } else if(userLives < 1 && CPULives < 1){
       $("h4,table,.end").remove();
-      $("body").append("<h1>It's A Tie!</h1>");
+      $("#playarea").append("<div class='endtext' id='endtie'><h1>It's A Tie!</h1></div>");
     } else {
         if(userLives < 1){
              $("h4,table,.end").remove();
-             $("body").append("<h1>"+ foe + " wins!</h1>");
+             $("#playarea").append("<div class='endtext' id='endfoe'><h1>"+ foe + " Wins!</h1></div>");
              setTimeout(opWin, waittime);
              $("#leftplayer").attr("src", "").attr("width", "0");
              $("#leftplayer").attr("src", "img/dead.PNG").attr("width", "200");
         } else if(CPULives < 1){
              $("h4,table,.end").remove();
-             $("body").append("<h1><b>"+ username + " wins!</b></h1>");
+             $("#playarea").append("<div class='endtext' id='enduser'><h1><b>"+ username + " Wins!</b></h1></div>");
              setTimeout(userWin, waittime);
              $("#rightplayer").attr("src", "").attr("width", "0");
              $("#rightplayer").attr("src", "img/dead.PNG").attr("width", "200");
